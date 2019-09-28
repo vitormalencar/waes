72977514e3950cd8c7c23ea67c33f293857f113f
vitormalencar <vitor.maia.alencar@gmail.com>
2019-09-28 20:11:56 -0300

    addin app container

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Provider } from "react-redux";
 
-function App() {
+import HighlightContainer from "./components/HighlightComponent";
+import ResultsContainer from "./components/Results/ResultsContainer";
+import HighLightSelectorContainer from "./components/Filter/HighLightFilterContainer";
+import ViewColorSelectorContainer from "./components/Filter/ViewColorSelectorContainer";
+import configureStore from "./store/ConfigureStore";
+
+import "./App.css";
+const store = configureStore();
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="app-wrapper">
+        <HighLightSelectorContainer />
+        <HighlightContainer />
+        <ViewColorSelectorContainer />
+        <ResultsContainer />
+      </div>
+    </Provider>
   );
-}
+};
 
 export default App;
